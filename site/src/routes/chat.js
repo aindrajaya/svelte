9a20082e65517c724d41265495538d418a128b6d
Conduitry <git@chor.date>
2021-08-04 14:21:01 -0400

    -> v3.42.1

--- site/src/routes/chat.js
+++ site/src/routes/chat.js
@@ -1,3 +1,7 @@
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
 export function get(req, res) {
 	res.writeHead(302, { Location: 'https://discord.gg/yy75DKs' });
 	res.end();
